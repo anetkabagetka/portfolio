c57bad6987b5a29887dbc36b4457923806e4e5f3
Aneta Bolibruchová <anebol01@easv365.dk>
2026-04-19 15:52:28 +0200

    link redesign

--- src/components/Folder.vue
+++ src/components/Folder.vue
@@ -1,14 +1,18 @@
 <template>
-    <Navigation />
     <div class="folder">
-        <img src="../assets/folder full.png" alt="folder full">
+        <nav class="folder-menu">
+            <RouterLink to="/" exact-active-class="active" class="home-link">home</RouterLink>
+            <RouterLink to="/about" exact-active-class="active" class="about-link">about</RouterLink>
+            <RouterLink to="/works" exact-active-class="active" class="works-link">works</RouterLink>
+        </nav>
+        <div class="folder-content">
+            <h2>Welcome!</h2>
+        </div>
     </div>
 </template>
 
 <script setup>
-import Navigation from './Navigation.vue';
+
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
