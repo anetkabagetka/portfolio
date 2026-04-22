15490f44aae761bbd556d258f3894524606dfbf4
Aneta Bolibruchová <anebol01@easv365.dk>
2026-04-22 22:51:13 +0200

    works spacing and shadows

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -1,11 +1,18 @@
 <template>
     <div class="work" v-for="work in works" :key="work.id"> 
-        <img :src="work.image" :alt="work.title">
-        <h3>{{ work.title }}</h3>
-        <p>{{ work.description }}</p>
+        <img :src="work.image" :alt="work.title" class="work-image">
+        <h3 class="work-title">{{ work.title }}</h3>
+        <p class="work-description">{{ work.description }}</p>
     </div>
 </template>
 
 <script setup>
 import { works } from '@/data/works.js';
 </script>
+
+<style scoped>
+.work {
+    box-shadow: 10px 16px 16px rgba(0, 0, 0, 0.2);
+    margin-top: 60px;
+}
+</style>
